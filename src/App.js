481b602854d6272133f7922a86e81fde83bd7bfb
AlexandreCSilva <alecosta.silva12@gmail.com>
2023-01-23 16:12:40 -0300

    feat: basic App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React  from 'react';
+import React from 'react';
+import Reset from './assets/Reset';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+import { Room } from './pages/Room';
 
 export function App() {
   return (
-    <div className="App">
-      aqui
-    </div>
+    <>
+      <Reset />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Room />} />
+          <Route index path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
